b0c370c18962e92bebbdb5d40e6d5c8655219cbc
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-07-11 18:15:19 -0300

    Change wkhtml download URL

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 default['wkhtmltox']['version'] = "0.12.2"
-default['wkhtmltox']['url'] = "http://downloads.sourceforge.net/project/wkhtmltopdf/archive/%{version}/wkhtmltox-%{version}_linux-%{codename}-%{arch}.deb"
+default['wkhtmltox']['url'] = "http://download.gna.org/wkhtmltopdf/0.12/%{version}/wkhtmltox-%{version}_linux-%{codename}-%{arch}.deb"
